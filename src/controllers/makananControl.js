e86c0b5b28d219146ff920182a639b535b353286
afatkhulamam <afatkhulamam@gmail.com>
2021-05-24 23:30:06 +0700

    delete data

--- src/controllers/makananControl.js
+++ src/controllers/makananControl.js
@@ -33,5 +33,26 @@ module.exports ={
         }catch(err){
             res.json({message: err})
         }
+    },
+    upgradeMakanan: async (req, res) => {
+        const id = req.params.makananId
+        try{
+            const makananUpdate = await Makanan.updateOne({_id: id}, {
+                nama: req.body.nama,
+                jenis: req.body.jenis
+            })
+            res.json(makananUpdate)
+        }catch(err){
+            res.json({message: err})
+        }
+    },
+    hapusMakanan: async (req, res) => {
+        const id = req.params.makananId
+        try{
+            const makananHapus = await Makanan.deleteOne({_id: id})
+            res.json(makananHapus)
+        }catch(err){
+            res.json({message: err})
+        }
     }
 }
--- src/routes/makananRoute.js
+++ src/routes/makananRoute.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const route = express.Router()
 
-const {inputMakanan, getMakanan, updateMakanan} = require('../controllers/makananControl')
+const {inputMakanan, getMakanan, updateMakanan, upgradeMakanan, hapusMakanan} = require('../controllers/makananControl')
 
 route.post('/input', inputMakanan)
 route.get('/get', getMakanan)
 route.patch('/update/:makananId', updateMakanan)
+route.put('/upgrade/:makananId', upgradeMakanan)
+route.delete('/hapus/:makananId', hapusMakanan)
 
 module.exports = route
